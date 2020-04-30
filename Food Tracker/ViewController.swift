e64ad30796cefd5c6466bbd9e764b67b35dc5f0d
Felix Jimoh <astrodextro@gmail.com>
2020-04-30 14:34:18 +0100

    Initial Commit

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Doci Healthcare on 30/04/2020.
+//  Copyright © 2020 Sopht Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
