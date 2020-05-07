6fea1b1fbeb05adc69f68be4a4509bdc71883cdf
Felix Jimoh <astrodextro@gmail.com>
2020-05-07 17:23:16 +0100

    Initial commit

---
+++ Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  Food Tracker
+//
+//  Created by Doci Healthcare on 06/05/2020.
+//  Copyright © 2020 Sopht Inc. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
